915cbeea856a863a6488892206a9ee2fed3407a8
elbit <elbitcat@gmail.com>
2013-01-23 22:21:17 +0100

    nous docs i bk preEm

---
+++ com.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>TekneCultura</title>
+    <meta name="description" content="Serveis per a projectes culturals per conèixer i connectar amb la seva comunitat">
+    <meta name="author" content="www.elbit.cat">
+    <meta name="version" content="0.5">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+
+    <link rel="stylesheet" href="css/estil.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <link rel="stylesheet" href="css/type.css" >
+    
+    <script src="js/vendor/modernizr-2.6.1.min.js"></script>
+</head>
+    
+    
+<div id="top" class="container" >
+
+     <header > 
+        
+          <div class="logo">
+            <a href="index.php"  alt="home"><img src="img/teknelogo-beta-0.4.png"></a>
+          </div>
+          
+         <nav class="menu" >
+              <a  href="que.php"> Què </a>
+              <a class="menu-active" href="com.php"> Com</a>
+              <a href="qui.php"> Qui</a>
+              <a href="blog.php"> Blog...</a>
+              <a href="mailto:info@teknecultura.com"> Contacte</a>
+          </nav>
+        
+      </header>
+    
+
---
+++ header.php
@@ -0,0 +1,15 @@
+  <header> 
+    
+      <div class="logo">
+        <a href="#"  alt="home"><img src="img/teknelogo-beta-0.4.png"></a>
+      </div>
+      
+      <nav class="menu" >
+          <a  href="que.php"> Què </a>
+          <a href="com.php"> Com</a>
+          <a href="qui.php"> Qui</a>
+          <a href="blog.php"> Blog...</a>
+          <a href="mailto:info@teknecultura.com"> Contacte</a>
+      </nav>
+    
+  </header>
