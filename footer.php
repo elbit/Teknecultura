5f00bdad21d8ee30b2fcd83235de0d9a22296308
elbit <elbitcat@gmail.com>
2013-02-07 20:17:51 +0100

    footer amb scripts

--- footer.php
+++ footer.php
@@ -1,29 +1,66 @@
-<footer class="well"> 
-    <ul class="modulo-footer address" itemscope itemtype="http://data-vocabulary.org/Organization">    <li></li>
-        <li itemprop="brand"><a href="http://teknecultura.com" itemprop="url"><img src="img/teknelogo-beta-0.4-small.png"></a></li>
-        <li itemprop="email">info@teknecultura.com</li>
-        <ul itemprop="address" itemscope itemtype="http://data-vocabulary.org/Address">
-          <li itemprop="street-address">Vidal i Ribas 25</li>
-          <li itemprop="locality">Sant Feliu de Llobregat</li>
-          <li itemprop="postal-code">08980</li> 
-          <li itemprop="region">Barcelona</li>
-          <li itemprop="country-name" class="hidden">Spain</li>
-        </ul>
-        <li itemprop="tel" href="939853242">93 985 32 42</li>
-    </ul>
+  <footer class="well"> 
+      <ul class="module-footer address" itemscope itemtype="http://data-vocabulary.org/Organization">    <li></li>
+          <li itemprop="brand"><a href="http://teknecultura.com" itemprop="url"><img src="img/teknelogo-beta-0.4-small.png"></a></li>
+          <li itemprop="email">info@teknecultura.com</li>
+          <ul itemprop="address" itemscope itemtype="http://data-vocabulary.org/Address">
+            <li itemprop="street-address">Vidal i Ribas 25</li>
+            <li itemprop="locality">Sant Feliu de Llobregat</li>
+            <li itemprop="postal-code">08980</li> 
+            <li itemprop="region">Barcelona</li>
+            <li itemprop="country-name" class="hidden">Spain</li>
+          </ul>
+          <li itemprop="tel" href="939853242">93 985 32 42</li>
+      </ul>
+      
+      <div class=" module-footer">
+        <nav class="menu menu-footer">
+            <a  href="que.php"> Què </a>
+            <a href="com.php"> Com</a>
+            <a href="qui.php"> Qui</a>
+            <a href="blog.php"> Bloc...</a>
+            <a href="mailto:info@teknecultura.com"> Contacte</a>
+            <a class="to-top" href="#top">&#11014; Puja &#11014;</a>
+        </nav>
+      </div>
+      
+      <div class="module-footer xarxes">
+      
+        <div class="navbar-inner cta cta-footer">
+
+             <form action="http://produccionscontrabaix.us5.list-manage.com/subscribe/post?u=df5f9aafe375682bc4f69b46d&amp;id=2405a54f28" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate navbar-form" target="_blank" novalidate>
+              <h2 class="cta-lema">Vols rebre informació?</h2>
+              <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="el teu e-mail" required>
+              <input type="submit" value="Subscriu-te" name="Subscriu-te" id="mc-embedded-subscribe" class="btn btn-info btn-block">
+            </form>
+       </div>
+        <!--  <a><img src="img/twitter-icon.png"></a> 
+        <a><img src="img/email-icon-small.png"></a> -->
+        <!-- <a><img src="img/facebook-logo.png"></a>  -->
+      </div>
+  </footer>
+  
+  
+</div> <!-- fi container -->
+
+
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.7.2.min.js"><\/script>')</script>
+    <script src="js/bootstrap-transition.js"></script>
+    <script src="js/bootstrap-carousel.js"></script>
+
     
-    <nav class="menu modulo-footer  menu-footer" >
-        <a  href="que.html"> Què </a>
-        <a href="com.html"> Com</a>
-        <a href="qui.html"> Qui</a>
-        <a href="blog.html"> Blog...</a>
-        <a href="mailto:info@teknecultura.com"> Contacte</a>
-        <a class="to-top" href="#top">Puja</a>
-    </nav>
-    
-    <nav class="modulo-footer xarxes">
-       <a><img src="img/twitter-icon.png"></a> 
-      <a><img src="img/email-icon-small.png"></a>
-      <!-- <a><img src="img/facebook-logo.png"></a>  -->
-    </nav>
-</footer>
+    <script type="text/javascript">
+
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-36720546-1']);
+      _gaq.push(['_setDomainName', 'teknecultura.com']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+
+    </script>
