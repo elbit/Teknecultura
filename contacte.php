da1749e34e48c3ca39e5683d3d2816e455540562
elbit <elbitcat@gmail.com>
2013-02-12 10:59:26 +0100

    creacio contacte.php + imgs

---
+++ contacte.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>TekneCultura</title>
+    <meta name="description" content="Serveis per a projectes culturals per conèixer i connectar amb la seva comunitat">
+    <meta name="author" content="www.elbit.cat">
+    <meta name="version" content="0.5">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="css/estil.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    
+    
+    <script src="js/vendor/modernizr-2.6.1.min.js"></script>
+</head>
+
+<body>
+    <!--[if lt IE 7]>
+        <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
+    <![endif]-->
+
+<div id="top" class="container" >
+
+  <header> 
+    
+      <div class="logo">
+        <a href="#"  alt="home"><img src="img/teknelogo-beta-0.6.png"></a>
+      </div>
+      
+      
+      <nav class="menu" >
+          <a href="que.php"> Què </a>
+          <a href="com.php"> Com</a>
+          <a href="qui.php"> Qui</a>
+          <a href="http://localhost:8888/wp%20test%20themes%20/"> Bloc...</a>
+          <a class="menu-active hidden-phone" href="contacte.php"> Contacte</a>
+      </nav>
+    
+  </header>
+  
+  <img src="img/contacte-0.3.png">
+  
+  <div>
+      <iframe width="300" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.es/maps?f=q&amp;source=s_q&amp;hl=es&amp;geocode=&amp;q=Carrer+Vidal+i+Ribas,+Sant+Feliu+de+Llobregat+25&amp;aq=&amp;sll=41.381241,2.046932&amp;sspn=0.010691,0.022016&amp;ie=UTF8&amp;hq=&amp;hnear=Carrer+Vidal+i+Ribas,+25,+08980+Sant+Feliu+de+Llobregat,+Barcelona&amp;t=m&amp;ll=41.381252,2.04689&amp;spn=0.01932,0.025749&amp;z=14&amp;iwloc=A&amp;output=embed"></iframe><br /><small><a href="https://maps.google.es/maps?f=q&amp;source=embed&amp;hl=es&amp;geocode=&amp;q=Carrer+Vidal+i+Ribas,+Sant+Feliu+de+Llobregat+25&amp;aq=&amp;sll=41.381241,2.046932&amp;sspn=0.010691,0.022016&amp;ie=UTF8&amp;hq=&amp;hnear=Carrer+Vidal+i+Ribas,+25,+08980+Sant+Feliu+de+Llobregat,+Barcelona&amp;t=m&amp;ll=41.381252,2.04689&amp;spn=0.01932,0.025749&amp;z=14&amp;iwloc=A" style="color:#0000FF;text-align:left">Veure mapa més gran</a></small>
+      </div>
+    
+    
+    
+    
+    
+  <?php include 'footer.php'; ?>
+
+
+</body>
+</html>
+  
