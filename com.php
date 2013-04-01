ff3bcd84f4fe299c306a45076c7f6143a332049c
elbit.cat <elbitcat@gmail.com>
2013-04-01 21:22:03 +0200

    tancar body tag :(

--- com.php
+++ com.php
@@ -18,7 +18,12 @@
     <script src="js/vendor/modernizr-2.6.1.min.js"></script>
 </head>
     
-    
+
+<body>
+    <!--[if lt IE 7]>
+        <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
+    <![endif]-->  
+
 <div id="top" class="container" >
 
   <header > 
@@ -37,39 +42,36 @@
   
   <article>
       <h1>TekneCultura: </h1>
-      <h2>és una solució de màrqueting</h2>
+      <h2>és una solució de màrqueting que...</h2>
       
       <section>
-        <ul class="unstyled-list">
+        <ul class="">
           <li>
-            <span class="important-second ">TekneCultura és la solució que permet disposar d’un departament especialitzat en màrqueting cultural,</span> 
-            però amb costos flexibles i adaptables a les necessitats, sense incrementar o sobrecarregar estructura actual, amb especialistes i les eines més innovadores.
+            <span class="important-second ">permet disposar d’un equip especialitzat, </span> amb costos flexibles i adaptables a les necessitats, sense incrementar o sobrecarregar estructura actual, amb especialistes i les eines més innovadores.
+
           </li>
           
           <li>
-            <span class="important-second ">TekneCultura complementa les noves eines i obté el màxim resultat i retorn de la inversió </span> 
-            dels nous sistemes de gestió de la venda d’entrades, o en les xarxes socials.
+            <span class="important-second ">complementa de les noves eines per obtenir el màxim resultat i retorn de la inversió </span> 
+            i en la seva implementació amb la metodologia del Pla de Continguts i Mètriques PCM.
           </li>
           
           <li>
-            <span class="important-second ">TekneCultura acompanya en la definició de l’estratègia de màrqueting relacional</span> i en la seva implementació amb la metodologia del Pla de Continguts i Mètriques PCM. 
+            <span class="important-second ">pot assumir una responsabilitat global o funcions específiques dins del pla de màrqueting:</span> gestió de contingut, email màrqueting, analítica web, analítica CRM i segmentació, gestió de la presència a xarxes socials i comunity management. 
           </li>
           <li>
-            <span class="important-second ">TekneCultura pot assumir una responsabilitat global o només funcions específiques dins del pla de màrqueting:</span> gestió de contingut, email màrqueting, analítica web, analítica CRM i segmentació, gestió de la presència a xarxes socials i comunity management.
+            <span class="important-second ">converteix dades en coneixement,</span> ntegra les diferents fonts d’informació, sistemes de tiqueting, d’email màrqueting, CRM, anlítica web... en una base de dades centralitzada, les analitza, elabora informes àgils, contribueix a la creació i gestió de coneixement sobre els usuaris i la comunitat.
+
           </li>
           
           <li>
-            <span class="important-second ">TekneCultura converteix dades en coneixement,</span> 
-            integra les diferents fonts d’informació, sistemes de tiqueting, d’email màrqueting, CRM, anlítica web... en una base de dades centralitzada, les analitza, elabora informes àgils, contribueix a la creació i gestió de coneixement sobre els usuaris i la comunitat.
-          </li>
-          <li>
-            <span class="important-second ">TekneCultura treballa amb plena integració organitzativa</span>
+            <span class="important-second ">treballa amb plena integració organitzativa</span> 
             amb el seu client, amb un gestor de compte com a contacte en tot moment, que coordina un equip multidisciplinari ad hoc que reuneix coneixements i capacitat analítica de màrqueting, així com expertesa tecnològica i creativitat.
           </li>
           <li>
-            <span class="important-second ">TekneCultura planifica, implementa, analitza i avalua en un procés de millora continua </span>
-            que garanteix millorar el coneixement i connexió dels projectes culturals amb la seva comunitat.
+            <span class="important-second ">planifica, implementa, analitza i avalua en un procés de millora continua</span> que garanteix millorar el coneixement i connexió dels projectes culturals amb la seva comunitat.
           </li>
+          
         
         </ul>
         </section>
@@ -85,92 +87,33 @@
         <h3 class="important" >Característiques del servei</h3>
         <ul >
           <li>atenció rebuda per un equip organitzat ad hoc per al client dirigit per un responsable del client per part de Teknecultura</li>
-          <li>en contacte permanent per detectar i atendre necessitats de màrqueting operatiu</li>
-          <li>disponibilitat de recursos directa i total, idèntica a una integració orgànica del servei</li>
-          <li>cost flexible i modular, de manera que es pot adaptar a necessitats i disponibilitats </li>
-          <li>innovació i alta especialització en el desenvolupament del màrqueting operatiu </li>
-          <li>millora continua i garantida en el procés de gestió de la informació: estratègia – implementació – anàlisi – revisió </li>
+          <li>cost flexible i modular, de manera que es pot adaptar a necessitats i disponibilitats</li>
+          <li>millora continua i garantida en el procés de gestió de la informació: estratègia – implementació – anàlisi – revisió</li>
+          
         </ul>
       </section>
       
       <section class="serveis">
         <h3 class="important">Descripció dels serveis</h3>
       
-        <h4>pla de Continguts i Mètriques PCM</h4>
+        
         <ul>
-          <li>Anàlisi de situació interna i externa</li>
-          <li>Definició de relat i túnel de conversió del projecte</li>
-          <li>Estratègia de continguts: Planificació i coordinació de continguts, de la seva difusió i de les mètriques de seguiment amb la metodologia Pla de Continguts i Mètriques PCM</li>
-          <li>Implementació estratègia i creació de continguts</li>
-          <li>Informes d’avaluació de l’eficàcia dels continguts, millora continua</li>
+          <li>Disseny, implementació, mètriques i avaluació de l’estratègia de Continguts</li>
+          <li>Gestió de serveis de tiqueting, bases de dades i CRM Màrqueting</li>
+          <li>Gestió e-mail màrqueting</li>
+          <li>Assessorament, disseny i analítica web i campanyes d’anuncis digitals</li>
+          <li>Gestió social media</li>
         </ul>
+
+        <a href="http://teknecultura.com/bloc/com/"  class="to-bloc">Explicació dels serveis més detallats en aquest article del bloc. →</a> 
       
-      
-     
-      
-      <h4>tiqueting i CRM Màrqueting</h4>
-      <ul>
-        <li>assessorament en la selecció de sistema de tiqueting adequat a dimensions i necessitats del projecte</li>
-        <li>suport i formació als usuaris del sistema</li>
-        <li>assessorament i acompanyament en el procés estratègic d’implantació de “filosofia CRM”, d’orientació a l’usuari</li>
-        <li>analítica base de dades CRM, segmentació</li>
-        <li>enquestes de satisfacció a peu d’escenari i amb focus grup</li>
-        <li>disseny i manteniment del procés de captació de dades coherent amb la LOPD</li>
-      </ul>
-     
-      
-      <h4>e-mail màrqueting</h4>
-      <ul>
-        <li>desenvolupament estratègia de continguts</li>
-        <li>elaboració continguts</li>
-        <li>disseny gràfic dels mails</li>
-        <li>enviament i seguiment obertures i clics</li>
-        <li>informes analítics d’obertures i de conversions</li>
-        <li>segmentació</li>
-        <li>anàlisi i propostes de millora continua</li>
-        <li>integració amb analítica web i anàlisi efecte sobre web</li>
-        <li>integració amb CRM i analítica efecte sobre compra</li>
-        <li>gestió de les dades i compliment de la LOPD</li>
-        <li>disseny i implementació d’estratègies de creixement de mailing</li>
-        <li>recollida, selecció, depuració i integració de les dades existents, des dels orígens disponibles a una base de dades única</li>
-      </ul>
-      
-      
-      <h4>web</h4>
-      <ul>
-        <li>assessorament en el disseny</li>
-        <li>disseny estratègia de keywords, posicionament SEO i SEM</li>
-        <li>disseny i gestió dinàmica campanyes google adds, youtube i anuncis digitals</li>
-        <li>analítica web i de resultats de campanyes d’anuncis digitals, informes analítics i propostes de millora continua</li>
-        <li>disseny estratègia continguts i suport a l’elaboració</li>
-      </ul>
-      
-      <h4>social media</h4>
-      <ul>
-        <li>anàlisi de comunitats, construïda i ideal. Anàlisi de prestigi i impacte social. Anàlisi de retorn directe des de xarxes socials.</li>
-        <li>disseny i implementació estratègies de construcció de comunitat</li>
-        <li>segmentació comunitat</li>
-        <li>escolta i dinamització activa segments de públics</li>
-        <li>atenció al client i gestió de crisis</li>
-        <li>disseny i implementació estratègies de contingut i de foment de la participació, i el compromís de marca per a xarxes socials i de redifusió d’altres continguts</li>
-        <li>elaboració i implementació estratègia de màrqueting relacional en l’entorn digital integrada en estratègia global de màrqueting</li>
-        <li>integració d’apps socials a webs, foment de la participació a la web des de xarxes socials, widgets socials</li>
-        <li>disseny i implementació anuncis a Facebook</li>
-        <li>disseny i manteniment fan page Facebook i pàgines de Twitter</li>
-        <li>disseny i implementació de campanyes de difusió en vídeo</li>
-       
-      </ul>
-    
     </section>
+
+
       
   </article>
   
-  
-
-
-
-   
-  <?php include 'footer.php'; ?> 
+<?php include 'footer.php'; ?> 
   
 </div> <!-- fi container -->
   
